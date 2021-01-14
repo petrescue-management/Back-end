bfb30fc5ec6506d3dd442f3a8e7ecc54d7a7adb1
PiimTran <66046271+PiimTran@users.noreply.github.com>
2021-01-14 09:44:06 +0700

    implement_update_center_registration_form_api

---
+++ PetRescue/PetRescue.Data/ViewModels/CenterRegistrationFormModels.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetRescue.Data.ViewModels
+{
+    public class UpdateCenterRegistrationFormModel
+    {
+        public Guid FormId { get; set; }
+        public string CenterName { get; set; }
+        public string CenterAddress { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Description { get; set; }
+        public int CenterRegisterStatus { get; set; }
+    }
+}
