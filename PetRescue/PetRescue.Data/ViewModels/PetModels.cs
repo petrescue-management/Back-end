db37b9c8a962b41c88a014c68c8e65b32777ecf7
PiimTran <66046271+PiimTran@users.noreply.github.com>
2021-01-22 11:09:44 +0700

    update_model_for_pet(s)

---
+++ PetRescue/PetRescue.Data/ViewModels/PetModels.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetRescue.Data.ViewModels
+{
+    public class PetModel
+    {
+        public Guid PetId { get; set; }
+        public string PetName { get; set; }
+        public int PetGender { get; set; }
+        public int? PetAge { get; set; }
+        public double Weight { get; set; }
+        public string Description { get; set; }
+        public Guid PetBreedId { get; set; }
+        public string PetBreedName { get; set; }
+        public Guid PetFurColorId { get; set; }
+        public string PetFurColorName { get; set; }
+        public bool IsVaccinated { get; set; }
+        public bool IsSterilized { get; set; }
+        public int PetStatus { get; set; }
+        public Guid CenterId { get; set; }
+    }
+}
---
+++ PetRescue/PetRescue.Data/ViewModels/PetProfileModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetRescue.Data.ViewModels
+{
+    public class PetProfileModel
+    {
+        public Guid PetId { get; set; }
+        public string PetName { get; set; }
+        public int PetGender { get; set; }
+        public int? PetAge { get; set; }
+        public double Weight { get; set; }
+        public string Description { get; set; }
+        public Guid PetBreedId { get; set; }
+        public Guid PetFurColorId { get; set; }
+        public bool IsVaccinated { get; set; }
+        public bool IsSterilized { get; set; }
+    }
+}
